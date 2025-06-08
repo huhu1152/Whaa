7c21afe01f135764bdf1532e40c7341a9903fa04
huhu1152 <152785027+huhu1152@users.noreply.github.com>
2025-06-08 19:43:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
 import tensorflow as tf
 import numpy as np
 import os
@@ -10,7 +9,7 @@ import requests
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-# تحميل النموذج من رابط خارجي
+# تحميل النموذج من Dropbox
 MODEL_URL = "https://www.dl.dropboxusercontent.com/scl/fi/bx919lqd3r3yvtsxi50ns/stone_classifier_model.h5?rlkey=q4xnvaiw1lm61xr5xcqbcoppy&st=7jqoakb2&dl=1"
 MODEL_PATH = "model.h5"
 
@@ -25,17 +24,18 @@ model = tf.keras.models.load_model(MODEL_PATH)
 class_names = ['يشب', 'ياقوت', 'جمشت', 'زمرد', 'فيروز', 'عقيق', 'اللؤلؤ الأبيض', 'اللؤلؤ الأسود']
 
 def preprocess_image(image_path):
-    img = Image.open(image_path).resize((224, 224))
+    img = Image.open(image_path).resize((224, 224)).convert('RGB')
     img_array = np.array(img) / 255.0
     return np.expand_dims(img_array, axis=0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        file = request.files['image']
+        file = request.files['file']  # تعديل هنا
         if file:
             filename = secure_filename(file.filename)
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)  # تأكد من وجود المجلد
             file.save(filepath)
 
             img_array = preprocess_image(filepath)
